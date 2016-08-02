939f5af674ac11d602335156555d9da17983c0f2
BrunoCau <brunocau@gsd.com.br>
2016-08-02 17:46:34 -0300

    removido bootstrap

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-require("./node_modules/bootstrap/dist/css/bootstrap.min.css")
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -8,7 +7,7 @@ export class App extends React.Component {
 			<div>
 				<h1>Jumbotron</h1>
 				<p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-				<p><a className="btn btn-primary btn-lg">Learn more</a></p>
+				<p><a>Learn more</a></p>
 			</div>
 		);
 	}
